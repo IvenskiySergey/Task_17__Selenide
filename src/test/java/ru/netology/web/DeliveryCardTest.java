00504b96c51b5397f1125d3d051ea05e6e3cbf8b
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-08-25 23:32:21 +0300

    Added generateDate method and clear date field, fixed selectors and checks

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -1,37 +1,37 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryCardTest {
 
     @Test
     void successfulCardOrderWithDelivery() {
-        Date date = new Date();
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date newDate = instance.getTime();
-        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
 
-        String datePlus3Days = formatForDate.format(newDate);
+        String planningDate = generateDate(4);
 
         open("http://localhost:9999/");
         $("span[data-test-id='city'] input").setValue("Севастополь");
-        $("span[data-test-id='date'] input[placeholder]").setValue(datePlus3Days);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("span[data-test-id='date'] input[placeholder]").setValue(planningDate);
         $("span[data-test-id='name'] input").setValue("Иван Иванов-Петров");
         $("span[data-test-id='phone'] input").setValue("+79854585033");
         $("span[class='checkbox__box']").click();
         $(".button_view_extra").click();
         $(".spin_size_m.spin_visible").shouldBe(visible);
-        $x("//*[contains(text(),'Встреча успешно забронирована на ')]").shouldBe(visible, Duration.ofSeconds(16));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
+    }
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
